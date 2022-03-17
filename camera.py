990bc15fed25ad6f31c23485e98a2ea8f5af8ca9
Owen Oertell <owenoertell@Owens-MacBook-Pro-3.local>
2022-03-16 22:15:41 -0400

    Adding

---
+++ camera.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as gp
+import os
+import cv2 as cv
+import numpy as np
+import time
+
+
+def choose_channel(index):
+    channel_info = adapter.get(index)
+    if channel_info == None:
+        print("Can't get the correct channel info")
+    os.system(channel_info["i2c_cmd"])  # i2c write
+    gpio_sta = channel_info["gpio_sta"]  # gpio write
+    gp.output(7, gpio_sta[0])
+    gp.output(11, gpio_sta[1])
+    gp.output(12, gpio_sta[2])
+
+
+adapter = {
+    "A": {
+        "i2c_cmd": "i2cset -y 0 0x70 0x00 0x04",
+        "gpio_stte": [0, 0, 1],
+    },
+    "B": {
+        "i2c_cmd": "i2cset -y 0 0x70 0x00 0x05",
+        "gpio_sta": [1, 0, 1],
+    },
+    "C": {
+        "i2c_cmd": "i2cset -y 0 0x70 0x00 0x06",
+        "gpio_sta": [0, 1, 0],
+    },
+    "D": {
+        "i2c_cmd": "i2cset -y 0 0x70 0x00 0x07",
+        "gpio_sta": [1, 1, 0],
+    },
+}
+
+camera = cv.VideoCapture(0)
+
+gp.setwarnings(False)
+gp.setmode(gp.BOARD)
+gp.setup(7, gp.OUT)
+gp.setup(11, gp.OUT)
+gp.setup(12, gp.OUT)
+
+choose_channel("A")
+ret, frame = camera.read()
+cv.imwrite("A.jpg", frame)
+
+choose_channel("B")
+ret, frame = camera.read()
+cv.imwrite("B.jpg", frame)
+
+choose_channel("C")
+ret, frame = camera.read()
+cv.imwrite("C.jpg", frame)
+
+choose_channel("D")
+ret, frame = camera.read()
+cv.imwrite("D.jpg", frame)
